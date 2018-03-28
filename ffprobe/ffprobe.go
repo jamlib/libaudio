7db521a50123354246c7df61ee6141b85df9cd0f
Taylor <taylor@yahoo.com>
2018-03-28 15:28:56 -0500

    add: ffprobe wrapper

---
+++ ffprobe/ffprobe.go
@@ -0,0 +1,98 @@
+package ffprobe
+
+import (
+  "bytes"
+  "os/exec"
+  "encoding/json"
+)
+
+type Data struct {
+  Streams            []*Stream   `json:"streams"`
+  Format             *Format     `json:"format"`
+}
+
+type Stream struct {
+  // common
+  Index              int         `json:"index"`
+  CodecName          string      `json:"codec_name"`
+  CodecLongName      string      `json:"codec_long_name"`
+  CodecType          string      `json:"codec_type"`
+  CodecTimeBase      string      `json:"codec_time_base"`
+  CodecTagString     string      `json:"codec_tag_string"`
+  CodecTag           string      `json:"codec_tag"`
+  // audio
+  Channels           int         `json:"channels"`
+  ChannelLayout      string      `json:"channel_layout"`
+  SampleFmt          string      `json:"sample_fmt"`
+  SampleRate         string      `json:"sample_rate"`
+  TimeBase           string      `json:"time_base"`
+  StartPts           int         `json:"start_pts"`
+  StartTime          string      `json:"start_time"`
+  DurationTs         uint64      `json:"duration_ts"`
+  Duration           string      `json:"duration"`
+  // audio MP3
+  BitRate            string      `json:"bit_rate"`
+  // audio FLAC
+  BitsPerRawSample   string      `json:"bits_per_raw_sample"`
+  // image
+  Width              int         `json:"width"`
+  Height             int         `json:"height"`
+  PixFmt             string      `json:"pix_fmt"`
+}
+
+type Format struct {
+  Filename           string      `json:"filename"`
+  NumStreams         int         `json:"nb_streams"`
+  NumPrograms        int         `json:"nb_programs"`
+  FormatName         string      `json:"format_name"`
+  FormatLongName     string      `json:"format_long_name"`
+  StartTime          float64     `json:"start_time,string"`
+  Duration           float64     `json:"duration,string"`
+  Size               string      `json:"size"`
+  BitRate            string      `json:"bit_rate"`
+  Score              int         `json:"probe_score"`
+  Tags               *Tags       `json:"tags"`
+}
+
+type Tags struct {
+  Album              string      `json:"album"`
+  Artist             string      `json:"artist"`
+  Comment            string      `json:"comment"`
+  Date               string      `json:"date"`
+  Genre              string      `json:"genre"`
+  Disc               string      `json:"disc"`
+  DiscTotal          string      `json:"disctotal"`
+  Encoder            string      `json:"encoder"`
+  Title              string      `json:"title"`
+  Track              string      `json:"track"`
+  TrackTotal         string      `json:"tracktotal"`
+}
+
+func GetData(filePath string) (*Data, error) {
+  data := &Data{}
+
+  // check that ffprobe is installed on system
+  bin, err := exec.LookPath("ffprobe")
+  if err != nil {
+    return data, err
+  }
+
+  cmd := exec.Command(
+    bin, "-v", "quiet", "-print_format", "json",
+    "-show_streams", "-show_format", filePath,
+  )
+  var out bytes.Buffer
+  cmd.Stdout = &out
+
+  err = cmd.Run()
+  if err != nil {
+    return data, err
+  }
+
+  err = json.Unmarshal(out.Bytes(), data)
+  if err != nil {
+    return data, err
+  }
+
+  return data, nil
+}
