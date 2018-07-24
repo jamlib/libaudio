06606eb9734f3800ea3aeae2eeb87b0e2b954945
Taylor <taylor@yahoo.com>
2018-07-24 15:44:32 -0500

    ffmpeg: add option to fix mp3 with invalid track length

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -1,6 +1,7 @@
 package ffmpeg
 
 import (
+  "os"
   "fmt"
   "bytes"
   "errors"
@@ -52,44 +53,87 @@ func (f *ffmpeg) OptimizeAlbumArt(input, output string) (string, error) {
     "-vf", "scale=500:-1", output }...)
 }
 
-// convert lossless to mp3
-func (f *ffmpeg) ToMp3(input, quality string, meta Metadata, output string) (string, error) {
-  a := []string{ "-i", input }
+type Mp3Config struct {
+  Input, Quality, Output string
+  Meta Metadata
+  Fix bool
+}
 
-  if len(meta.Artwork) > 0 {
-    a = append(a, "-i", meta.Artwork)
-  }
+// mp3 quality helper function
+func (f *ffmpeg) mp3Quality(q string) []string {
+  a := []string{}
 
-  // mp3 audio codec
-  a = append(a, "-map", "0:a", "-c:a")
-  if quality == "copy" {
+  if q == "copy" {
     a = append(a, "copy")
   } else {
     a = append(a, "libmp3lame")
 
     // mp3 audio quality
-    if quality == "320" {
+    if q == "320" {
       a = append(a, "-b:a", "320k")
     } else {
       a = append(a, "-qscale:a", "0")
     }
   }
 
+  return a
+}
+
+// convert to mp3
+func (f *ffmpeg) ToMp3(c *Mp3Config) (string, error) {
+  a := []string{ "-i" }
+
+  // if track length displays outrageous number like 1035:36:51
+  // copy w/o metadata, then add metadata fixes it
+  fixOut := c.Output[:len(c.Output)-4] + "-fix.mp3"
+  if c.Fix {
+    b := []string{ "-i", c.Input, "-map_metadata", "-1", "-c:a" }
+    b = append(b, f.mp3Quality(c.Quality)...)
+    b = append(b, "-y", fixOut)
+
+    s, err := f.Exec(b...)
+    if err != nil {
+      return s, err
+    }
+
+    // next input is fixed output
+    a = append(a, fixOut)
+
+    // do not need to convert again
+    c.Quality = "copy"
+  } else {
+    a = append(a, c.Input)
+  }
+
+  if len(c.Meta.Artwork) > 0 {
+    a = append(a, "-i", c.Meta.Artwork)
+  }
+
+  // mp3 audio codec
+  a = append(a, "-map", "0:a", "-c:a")
+  a = append(a, f.mp3Quality(c.Quality)...)
+
   // id3v2 metadata
   a = append(a, "-id3v2_version", "4")
-  a = append(a, "-metadata", "artist=" + meta.Artist)
-  a = append(a, "-metadata", "album=" + meta.Album)
-  a = append(a, "-metadata", "disc=" + meta.Disc)
-  a = append(a, "-metadata", "track=" + meta.Track)
-  a = append(a, "-metadata", "title=" + meta.Title)
-  a = append(a, "-metadata", "date=" + meta.Date)
+  a = append(a, "-metadata", "artist=" + c.Meta.Artist)
+  a = append(a, "-metadata", "album=" + c.Meta.Album)
+  a = append(a, "-metadata", "disc=" + c.Meta.Disc)
+  a = append(a, "-metadata", "track=" + c.Meta.Track)
+  a = append(a, "-metadata", "title=" + c.Meta.Title)
+  a = append(a, "-metadata", "date=" + c.Meta.Date)
 
   // embedd album artwork
-  if len(meta.Artwork) > 0 {
+  if len(c.Meta.Artwork) > 0 {
     a = append(a, "-map", "1:v", "-c:v", "copy", "-metadata:s:v",
       "title=Album cover", "-metadata:s:v", "comment=Cover (Front)")
   }
 
-  a = append(a, "-y", output)
-  return f.Exec(a...)
+  a = append(a, "-y", c.Output)
+  s, err := f.Exec(a...)
+
+  if c.Fix && err == nil {
+    err = os.Remove(fixOut)
+  }
+
+  return s, err
 }
