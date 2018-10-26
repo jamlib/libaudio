945b94d902512c8ce78a7d72aa1236eec77f3222
Taylor <taylor@yahoo.com>
2018-10-26 11:10:14 -0500

    fsutil: improve inline comments, minor refactors

--- fsutil/fsutil.go
+++ fsutil/fsutil.go
@@ -45,6 +45,7 @@ func BundleFiles(dir string, files []string, f func(bundle []int) error) error {
   return nil
 }
 
+// copy a file from srcPath to destPath
 func CopyFile(srcPath, destPath string) (err error) {
   srcFile, err := os.Open(srcPath)
   if err != nil {
@@ -67,14 +68,18 @@ func CopyFile(srcPath, destPath string) (err error) {
   return
 }
 
+// returns slice of all nested audio files
 func FilesAudio(dir string) []string {
   return FilesByExtension(dir, AudioExts)
 }
 
+// returns slice of all nested image files
 func FilesImage(dir string) []string {
   return FilesByExtension(dir, ImageExts)
 }
 
+// returns string slice of all nested files within path that match certain
+// file extensions.
 func FilesByExtension(dir string, exts []string) []string {
   files := []string{}
 
@@ -104,27 +109,27 @@ func FilesByExtension(dir string, exts []string) []string {
   if err != nil {
     return []string{}
   }
-  // must sort: nested directories' files list first
-  // char / sorts before A-Za-z0-9
+
+  // sort: nested files list first since char / sorts before A-Za-z0-9
   sort.Strings(files)
 
   return files
 }
 
-// true if destination does not exist or src has larger file size
-func IsLarger(file, newFile string) bool {
-  f, err := os.Open(file)
+// returns true if dest does not exist or src has larger file size than dest
+func IsLarger(src, dest string) bool {
+  f, err := os.Open(src)
   defer f.Close()
   if err != nil {
     return false
   }
 
-  f2, err := os.Open(newFile)
+  f2, err := os.Open(dest)
   defer f2.Close()
 
   if err == nil {
-    fn, err := NthFileSize([]string{ file, newFile }, false)
-    if err != nil || fn == newFile {
+    fn, err := NthFileSize([]string{ src, dest }, false)
+    if err != nil || fn == dest {
       return false
     }
   }
@@ -132,72 +137,74 @@ func IsLarger(file, newFile string) bool {
   return true
 }
 
-// if dest folder already exists, merge audio file if Disc/Track not already present
-// within. merge all images currently not present
+// if dest folder exists, merge audio files that are not already present.
+// indexFunc() should return a disc number and a track title which is stored
+// in a lookup and used to determine if next audio file already exists.
 func MergeFolder(src, dest string,
   indexFunc func(f string) (int, string)) (string, error) {
 
-  // if folder already exists
+  // if dest folder does not exist, simply rename folder
   _, err := os.Stat(dest)
-  if err == nil {
-    // build dest audio file info maps
-    destAudios := FilesAudio(dest)
-    lookup := make(map[int]string, len(destAudios))
-    for _, destFile := range destAudios {
-      index, title := indexFunc(destFile)
-      lookup[index] = title
-    }
-
-    // copy only src audio files that don't already exist
-    copied := false
-    for _, srcFile := range FilesAudio(src) {
-      index, title := indexFunc(srcFile)
-      if _, found := lookup[index]; !found {
-        srcPath := filepath.Join(src, srcFile)
-
-        // if not found, copy audio file
-        _, f := filepath.Split(srcFile)
-        err = CopyFile(srcPath, filepath.Join(dest, f))
-        if err != nil {
-          return dest, err
-        }
-
-        // add to lookup, ensure copied is true
-        lookup[index] = title
-        copied = true
-
-        // remove source audio file
-        err = os.Remove(srcPath)
-        if err != nil {
-          return dest, err
-        }
-      }
-    }
-
-    // copy all image files (if copied at least one audio file)
-    if copied {
-      for _, imgFile := range FilesImage(src) {
-        imgPath := filepath.Join(src, imgFile)
-        _, img := filepath.Split(imgFile)
-        _ = CopyFile(imgPath, filepath.Join(dest, img))
-      }
-    }
-
-    // if remaining audio files, rename to folder (x)
-    if len(FilesAudio(src)) > 0 {
-      return RenameFolder(src, dest)
-    }
-
-    // else delete folder
-    err = os.RemoveAll(src)
-    return dest, err
+  if err != nil {
+    return RenameFolder(src, dest)
   }
 
-  // folder doesn't exist
-  return RenameFolder(src, dest)
+  // build dest audio file info maps
+  destAudios := FilesAudio(dest)
+  lookup := make(map[int]string, len(destAudios))
+  for _, destFile := range destAudios {
+    index, title := indexFunc(destFile)
+    lookup[index] = title
+  }
+
+  // boolean to determine if any audio files were copied
+  copied := false
+
+  // copy only src audio files that don't already exist
+  for _, srcFile := range FilesAudio(src) {
+    index, title := indexFunc(srcFile)
+    if _, found := lookup[index]; !found {
+      srcPath := filepath.Join(src, srcFile)
+
+      // if not found, copy audio file
+      _, f := filepath.Split(srcFile)
+      err = CopyFile(srcPath, filepath.Join(dest, f))
+      if err != nil {
+        return dest, err
+      }
+
+      // add to lookup, ensure copied is true
+      lookup[index] = title
+      copied = true
+
+      // remove source audio file
+      err = os.Remove(srcPath)
+      if err != nil {
+        return dest, err
+      }
+    }
+  }
+
+  // copy all image files (if copied at least one audio file)
+  if copied {
+    for _, imgFile := range FilesImage(src) {
+      imgPath := filepath.Join(src, imgFile)
+      _, img := filepath.Split(imgFile)
+      _ = CopyFile(imgPath, filepath.Join(dest, img))
+    }
+  }
+
+  // if remaining audio files, rename to dest folder by including (x)
+  if len(FilesAudio(src)) > 0 {
+    return RenameFolder(src, dest)
+  }
+
+  // only junk remains in src, delete it
+  err = os.RemoveAll(src)
+  return dest, err
 }
 
-// index of smallest/largest file in slice of files
+// return path of smallest/largest file by file size in slice of files
 func NthFileSize(files []string, smallest bool) (string, error) {
   sizes := []int64{}
 
@@ -227,12 +234,12 @@ func NthFileSize(files []string, smallest bool) (string, error) {
   return files[found], nil
 }
 
-// rename folder src to dest. if dest already exist, append (x)
-// to folder name, iterate until folder not found
+// rename folder src to dest. if dest already exists, append (x)
+// to folder name, increment (x) until folder path not found
 func RenameFolder(src, dest string) (string, error) {
+  // check if dest not found
   _, err := os.Stat(dest)
   if err == nil {
-    // increment (x) until dir not found
     x := 0
     for {
       x += 1
@@ -246,13 +253,13 @@ func RenameFolder(src, dest string) (string, error) {
     }
   }
 
-  // create parent dir by trimming via filepath.Dir
+  // create all parent directories
   err = os.MkdirAll(filepath.Dir(dest), 0777)
   if err != nil {
     return dest, err
   }
 
-  // rename to dest
+  // rename src to dest
   err = os.Rename(src, dest)
   return dest, err
 }
